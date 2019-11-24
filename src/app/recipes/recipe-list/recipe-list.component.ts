fb6f3fa663ce31925564b753349d3e02e175104a
John Wang <wangqianjiang@live.com>
2019-11-24 08:31:29 -0600

    Add recipe.model blue-print

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A tet Recipe', 'this is simply a test', 'http://i.huffpost.com/gen/1019781/images/o-BALSAMIC-RECIPE-facebook.jpg'),
+    new Recipe('Pizza', 'Large Pizza','http://www.seriouseats.com/images/2016/06/2016014-tomato-recipes-roundup-13.jpg')
+  ];
 
   constructor() { }
 
